699ed2a98956f9b39a9646a7158d6265c008f005
Tenzing Shrestha <tenzingtheking85@gmail.com>
2020-06-26 19:51:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,30 +4,30 @@ function basicTeenager(age) {
   }
 }
 
-function teenager(age) {
+function teenager(age){
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else {
     return "You are not a teenager";
   }
- }
+}
 
-function ageChecker(age) {
+function ageChecker(age){
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
-  } else if (age <=12) {
+  } else if (age < 13) {
     return "You are a kid";
   } else {
     return "You are a grownup";
   }
 }
 
-function ternaryTeenager(age) {
-    return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
+function ternaryTeenager(age){
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
 }
 
-function switchAge(age) {
-  swith(age){
+function switchAge(age){
+  switch (age) {
   case 13:
   case 14:
   case 15:
@@ -35,8 +35,8 @@ function switchAge(age) {
   case 17:
   case 18:
   case 19:
-    return "You are a teenager!";
+    return "You are a teenager";
   default:
-    return "You have an age";
-}
+    return "You have an age"
+  }
 }
